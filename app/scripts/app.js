9a8b64526f47bd4fd178c94051bf56b1e44752c7
Saurav Chatterjee <saurav.chatterjee@emids.com>
2017-06-06 12:01:31 +0530

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yapp
+ * @description
+ * # yapp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yapp', [
+    'ui.router',
+    'ngAnimate'
+  ])
+  .config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.when('/dashboard', '/dashboard/overview');
+    $urlRouterProvider.otherwise('/login');
+
+    $stateProvider
+      .state('base', {
+        abstract: true,
+        url: '',
+        templateUrl: 'views/base.html'
+      })
+        .state('login', {
+          url: '/login',
+          parent: 'base',
+          templateUrl: 'views/login.html',
+          controller: 'LoginCtrl'
+        })
+        .state('dashboard', {
+          url: '/dashboard',
+          parent: 'base',
+          templateUrl: 'views/dashboard.html',
+          controller: 'DashboardCtrl'
+        })
+          .state('overview', {
+            url: '/overview',
+            parent: 'dashboard',
+            templateUrl: 'views/dashboard/overview.html'
+          })
+          .state('profile', {
+            url: '/profile',
+            parent: 'dashboard',
+            templateUrl: 'views/dashboard/profile.html'
+          })
+          .state('register', {
+            url: '/register',
+            parent: 'base',
+            templateUrl: 'views/register.html'
+          });
+
+  });
+
