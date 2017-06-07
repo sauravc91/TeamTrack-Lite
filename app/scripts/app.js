628ebe357716ae00b4861315a3a8c977c7967760
Saurav Chatterjee <saurav.chatterjee@emids.com>
2017-06-07 12:11:54 +0530

    modified routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,34 +24,66 @@ angular
         url: '',
         templateUrl: 'views/base.html'
       })
-        .state('login', {
-          url: '/login',
-          parent: 'base',
-          templateUrl: 'views/login.html',
-          controller: 'LoginCtrl'
-        })
-        .state('dashboard', {
-          url: '/dashboard',
-          parent: 'base',
-          templateUrl: 'views/dashboard.html',
-          controller: 'DashboardCtrl'
-        })
-          .state('overview', {
-            url: '/overview',
-            parent: 'dashboard',
-            templateUrl: 'views/dashboard/overview.html'
-          })
-          .state('profile', {
-            url: '/profile',
-            parent: 'dashboard',
-            templateUrl: 'views/dashboard/profile.html'
-          })
-          .state('register', {
-            url: '/register',
-            parent: 'base',
-            templateUrl: 'views/register.html',
-            controller: 'SignUpCtrl'
-          });
+
+      //Route for login
+      .state('login', {
+        url: '/login',
+        parent: 'base',
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+
+      //Route for sign up
+      .state('register', {
+        url: '/register',
+        parent: 'base',
+        templateUrl: 'views/register.html',
+        controller: 'SignUpCtrl'
+      })
+    
+      //User dashboard base
+      .state('user', {
+        url: '/user',
+        parent: 'base',
+        templateUrl: 'views/dashboard.html',
+        controller: 'DashboardCtrl'
+      })
+
+      //User OverView
+      .state('overview', {
+        url: '/overview',
+        parent: 'user',
+        templateUrl: 'views/dashboard/overview.html'
+      })
+
+      //User profile
+      .state('profile', {
+        url: '/profile',
+        parent: 'user',
+        templateUrl: 'views/dashboard/profile.html'
+      })
+
+      //Admin dashboard base
+      .state('admin', {
+        url: '/admin',
+        parent: 'base',
+        templateUrl: 'views/admin.html',
+        //controller: 'AdminCtrl'
+      })
+
+      //Admin overview
+      .state('admin-overview', {
+        url: '/overview',
+        parent: 'admin',
+        templateUrl: 'views/admin/overview.html'
+      })
+
+      //Admin Profile
+      .state('admin-profile', {
+        url: '/profile',
+        parent: 'admin',
+        templateUrl: 'views/admin/profile.html'
+      });
 
   });
 
